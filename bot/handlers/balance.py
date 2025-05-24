1df43a03cfb5964ac5d6ff99f856de43df2aa81d
SKYFALL <arsyakost@gmail.com>
2025-05-24 23:08:27 +0500

    q

--- bot/handlers/balance.py
+++ bot/handlers/balance.py
@@ -167,9 +167,8 @@ async def start_crypto_payment(call: CallbackQuery):
     }
 
     networks_required = {
-        "USDT": "TRC20",       # Tether на сети Tron
+        "USDT": "TRON",       # Tether на сети Tron
         "USDC": "TRC20",       # USD Coin на сети Tron
-        "DAI": "BSC",          # DAI на сети Binance Smart Chain
         "ETH": "ARBITRUM",     # Ethereum на сети Arbitrum
         "BNB": "BSC",          # Binance Coin на сети Binance Smart Chain
         "LTC": "LTC",          # Litecoin на собственной сети
