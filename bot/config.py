c1d0173340f0cdeaf5092e2f8138f64c2e2ece92
ArsKostromin <arsyakost@gmail.com>
2025-09-29 21:45:33 +0500

    Update BASE_BACKEND_URL to point to anonixvpn.com for webhook integration

--- bot/config.py
+++ bot/config.py
@@ -27,7 +27,7 @@ def load_config() -> Config:
 
 CRYPTOMUS_API_KEY = "WwNQW5SvFmkwozP6JTetW1VCpo5ywjoZ0DbfEgM9GfkVaXj5VS1Ey4TwPzsaUEgvQcNi7ldIhtcNF6ZchEYtIKqUFRjw8R3qkJMN9G9VB3V6vtdd0XW0dxKotU9fvtcE"
 CRYPTOMUS_MERCHANT_UUID = "59fc86a1-d195-4df8-8d17-3d6b06d2fe48"
-BASE_BACKEND_URL = "https://admin.byebyefbi.com/payments/api/crypto/webhook/"
+BASE_BACKEND_URL = "https://admin.anonixvpn.com/payments/api/crypto/webhook/"
 
 
 CHANNEL_USERNAME = "@anonix_vpn"
